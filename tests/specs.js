735b4b0ee828d2380bae85e87dc0dc4b3abab47b
Matt <matt.snell.00@hotmail.co.uk>
2020-04-21 22:47:39 +0000

    add test for getDifficultyValue to specs.js

--- tests/specs.js
+++ tests/specs.js
@@ -1,14 +1,16 @@
 describe("randomNumber", function () {
-    describe("hard is false test", function () {
-        it("should return a random number between 0 and 3", function () {
-            expect(randomNumber(4)).toBeGreaterThanOrEqual(0);
-            expect(randomNumber(4)).toBeLessThanOrEqual(3);
-        });
+    it("should return a random number between 0 and 3 if hard is false", function () {
+        expect(randomNumber(4)).toBeGreaterThanOrEqual(0);
+        expect(randomNumber(4)).toBeLessThanOrEqual(3);
     });
-    describe("hard is true test", function () {
-        it("should return a random number between 0 and 4", function () {
-            expect(randomNumber(5)).toBeGreaterThanOrEqual(0);
-            expect(randomNumber(5)).toBeLessThanOrEqual(4);
-        });
+    it("should return a random number between 0 and 4 if hard is true", function () {
+        expect(randomNumber(5)).toBeGreaterThanOrEqual(0);
+        expect(randomNumber(5)).toBeLessThanOrEqual(4);
+    });
+});
+
+describe("getDifficultyValue", function() {
+    it("should return 4 (hard is set to false by default)", function() {
+        expect(getDifficultyValue()).toBe(4);
     });
 });
